c626030dbe5bd759d4ff601267d169b9ec450113
audiph <lagarde.marcjoseph@gmail.com>
2024-04-02 20:56:06 +0800

    base hero product and minor additions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,83 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  animate,
+} from 'framer-motion';
+import { useEffect } from 'react';
+
+const COLORS = ['#13FFA', '#1E67C6', '#CE84CF', '#DD335C'];
+
 function App() {
+  const color = useMotionValue(COLORS[0]);
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 100%, transparent 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+  useEffect(() => {
+    animate(color, COLORS, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror',
+    });
+  }, [color]);
+
   return (
     <>
-      <div>HOME</div>
+      <motion.section
+        style={{ backgroundImage }}
+        className="flex w-full h-screen flex-col items-center justify-center gap-2 px-4"
+      >
+        <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] text-balance">
+          Tic Tac Toe
+        </h1>
+        <span className="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
+          Classic Tic Tac Toe game for 2 players
+        </span>
+        <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+          <Dialog>
+            <DialogTrigger asChild>
+              <motion.button
+                style={{ border, boxShadow }}
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
+              >
+                Play
+              </motion.button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              {/* Content */}
+              <div className="grid gap-4 py-4">TEST</div>
+              <DialogFooter>
+                <Button type="submit">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <a
+            href="#score-board"
+            className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
+          >
+            View Scoreboard
+          </a>
+        </div>
+      </motion.section>
     </>
   );
 }
