8aa5ee45fe150084d59a115ed5dfd2937daac654
audiph <lagarde.marcjoseph@gmail.com>
2024-04-04 16:42:20 +0800

    modify boilerplate and setup server

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+/* tslint:disable-next-line */
+require('dotenv').config();
+import * as http from 'http';
+import { DATABASE_URL, PORT } from './var/config';
+import app from './server';
+
+const server: http.Server = http.createServer(app);
+
+server.listen(PORT);
+
+server.on('error', (e: Error) => {
+  console.log('Error starting server' + e);
+});
+
+server.on('listening', () => {
+  if (DATABASE_URL) {
+    console.log(
+      `Server started on port ${PORT} on env ${
+        process.env.NODE_ENV || 'dev'
+      } dbcon ${DATABASE_URL}`
+    );
+  } else {
+    console.log(
+      `Server started on port ${PORT} on env ${process.env.NODE_ENV || 'dev'}`
+    );
+  }
+});
+
+export default {
+  server,
+};
---
+++ backend/src/var/config.ts
@@ -0,0 +1,4 @@
+export const { DATABASE_URL, PORT } = process.env;
+
+export const ROUTES_DIR = './dist/routes/**/*.js';
+export const MODELS_DIR = './dist/models/**/*.js';
