d31d30eb70aaaa14f26fc61736b8d23ca679c164
audiph <lagarde.marcjoseph@gmail.com>
2024-04-04 17:58:37 +0800

    complete backend implementation

--- backend/src/controllers/games.server.controller.ts
+++ backend/src/controllers/games.server.controller.ts
@@ -3,11 +3,46 @@ import { Request, Response } from 'express';
 import { Games, GamesInput, GamesSchema } from '../models/games.model';
 
 export default class GamesController {
-  public getAllGames(req: Request, res: Response): void {
-    res.status(200).send({ message: 'Hello Worlds!' });
+  public async getAllGames(_: Request, res: Response) {
+    try {
+      const games = await Games.find();
+      res.status(200).send({
+        games,
+      });
+    } catch (error) {
+      res.status(500).send({ message: 'Error fetching games.', error });
+    }
   }
 
-  public async createGame(req: Request, res: Response): Promise<void> {
+  public async getGameById(req: Request, res: Response) {
+    try {
+      const game = await Games.findById(req.params.id)
+        .then((result) => result)
+        .catch((err) => {
+          return res.status(404).send({ message: 'Game not found.', err });
+        });
+
+      res.status(200).send({ game });
+    } catch (error) {
+      res.status(500).send({ message: 'Error fetching game.', error });
+    }
+  }
+
+  public async deleteGame(req: Request, res: Response) {
+    try {
+      const game = await Games.findByIdAndDelete(req.params.id)
+        .then((result) => result)
+        .catch((err) => {
+          return res.status(404).send({ message: 'Game not found.', err });
+        });
+
+      res.status(200).send({ game });
+    } catch (error) {
+      res.status(500).send({ message: 'Error updating game.', error });
+    }
+  }
+
+  public async createGame(req: Request, res: Response) {
     try {
       const { player_one, player_two }: GamesInput = req.body;
 
