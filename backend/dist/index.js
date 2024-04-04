68e64802ead20165855b6e44cfb6c6cd739d39cb
audiph <lagarde.marcjoseph@gmail.com>
2024-04-05 07:41:40 +0800

    v3

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,22 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/* tslint:disable-next-line */
+// Required environmental variables and configuration
 require('dotenv').config();
-const http = require("http");
-const awsServerlessExpress = require("aws-serverless-express");
-const bodyParser = require("body-parser");
 const express = require("express");
-const logger = require("morgan");
-const path = require("path");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet_1 = require("helmet");
-const config_1 = require("./var/config");
+const logger = require("morgan");
+const path = require("path");
 const helpers_1 = require("./helpers");
 const database_1 = require("./database");
+const config_1 = require("./var/config");
 const app = express();
+// Database Connection
 if (config_1.DATABASE_URL) {
     (0, database_1.default)(config_1.DATABASE_URL);
 }
+// Middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,30 +24,14 @@ app.use(express.json());
 app.use((0, helmet_1.default)());
 app.use(helmet_1.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(cors());
-const routes = (0, helpers_1.globFiles)(config_1.ROUTES_DIR);
-for (const model of (0, helpers_1.globFiles)(config_1.MODELS_DIR)) {
-    require(path.resolve(model));
-}
-for (const route of routes) {
+// Models & Routes
+(0, helpers_1.globFiles)(config_1.MODELS_DIR).forEach((model) => require(path.resolve(model)));
+(0, helpers_1.globFiles)(config_1.ROUTES_DIR).forEach((route) => {
     const { default: Route } = require(path.resolve(route));
-    const _ = new Route(app);
-}
-const server = http.createServer(app);
-const serverless = awsServerlessExpress.createServer(app);
-serverless.listen(config_1.PORT);
-serverless.on('error', (e) => {
-    console.log('Error starting server' + e);
+    new Route(app);
 });
-serverless.on('listening', () => {
-    if (config_1.DATABASE_URL) {
-        console.log(`Server started on port ${config_1.PORT} on env ${process.env.NODE_ENV || 'dev'} dbcon ${config_1.DATABASE_URL}`);
-    }
-    else {
-        console.log(`Server started on port ${config_1.PORT} on env ${process.env.NODE_ENV || 'dev'}`);
-    }
-});
-exports.handler = (event, context) => {
-    awsServerlessExpress.proxy(serverless, event, context);
+// Vercel compatible serverless function export
+exports.default = (req, res) => {
+    app(req, res);
 };
-exports.default = serverless;
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,23 @@
-/* tslint:disable-next-line */
+// Required environmental variables and configuration
 require('dotenv').config();
-import * as http from 'http';
-import * as awsServerlessExpress from 'aws-serverless-express';
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import * as bodyParser from 'body-parser';
 import * as express from 'express';
-import * as logger from 'morgan';
-import * as path from 'path';
+import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import Helmet from 'helmet';
-import { DATABASE_URL, PORT, MODELS_DIR, ROUTES_DIR } from './var/config';
+import * as logger from 'morgan';
+import * as path from 'path';
 import { globFiles } from './helpers';
 import connect from './database';
+import { DATABASE_URL, MODELS_DIR, ROUTES_DIR } from './var/config';
 
 const app: express.Express = express();
 
+// Database Connection
 if (DATABASE_URL) {
   connect(DATABASE_URL);
 }
 
+// Middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,43 +26,14 @@ app.use(Helmet());
 app.use(Helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(cors());
 
-const routes = globFiles(ROUTES_DIR);
-
-for (const model of globFiles(MODELS_DIR)) {
-  require(path.resolve(model));
-}
-
-for (const route of routes) {
+// Models & Routes
+globFiles(MODELS_DIR).forEach((model) => require(path.resolve(model)));
+globFiles(ROUTES_DIR).forEach((route) => {
   const { default: Route } = require(path.resolve(route));
-  const _ = new Route(app);
-}
-
-const server: http.Server = http.createServer(app);
-
-const serverless = awsServerlessExpress.createServer(app);
-
-serverless.listen(PORT);
-
-serverless.on('error', (e: Error) => {
-  console.log('Error starting server' + e);
+  new Route(app);
 });
 
-serverless.on('listening', () => {
-  if (DATABASE_URL) {
-    console.log(
-      `Server started on port ${PORT} on env ${
-        process.env.NODE_ENV || 'dev'
-      } dbcon ${DATABASE_URL}`
-    );
-  } else {
-    console.log(
-      `Server started on port ${PORT} on env ${process.env.NODE_ENV || 'dev'}`
-    );
-  }
-});
-
-exports.handler = (event: APIGatewayProxyEvent, context: Context) => {
-  awsServerlessExpress.proxy(serverless, event, context);
+// Vercel compatible serverless function export
+export default (req: express.Request, res: express.Response) => {
+  app(req, res);
 };
-
-export default serverless;
