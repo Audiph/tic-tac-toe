2614b46e82d47cd1bdc667fc4a2ba23def38049c
audiph <lagarde.marcjoseph@gmail.com>
2024-04-05 00:13:26 +0800

    delete vercel configs

--- backend/src/helpers/index.js
+++
@@ -1,6 +0,0 @@
-import { sync } from 'glob';
-import { union } from 'lodash';
-export const globFiles = (location) => {
-    return union([], sync(location));
-};
-//# sourceMappingURL=index.js.map
--- backend/src/var/config.js
+++
@@ -1,4 +0,0 @@
-export const { DATABASE_URL, PORT } = process.env;
-export const ROUTES_DIR = './dist/routes/**/*.js';
-export const MODELS_DIR = './dist/models/**/*.js';
-//# sourceMappingURL=config.js.map
