7e562fb59355551d6603d07e049a6b3f32177dc7
audiph <lagarde.marcjoseph@gmail.com>
2024-04-05 09:27:40 +0800

    v1

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -1,8 +1,24 @@
+require('dotenv').config();
+import bodyParser from 'body-parser';
 import express, { Application } from 'express';
-import serverless from 'serverless-http';
+import logger from 'morgan';
+import cors from 'cors';
+import Helmet from 'helmet';
+import dbConnect from '../../src/database';
 import GameRoutes from '../../src/routes/games.server.route';
+import serverless from 'serverless-http';
 
 const api: Application = express();
+
+dbConnect();
+
+api.use(logger('dev'));
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: false }));
+api.use(express.json());
+api.use(Helmet());
+api.use(Helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+api.use(cors());
 
 api.use('/api/v1', GameRoutes);
 
